06da339517f528b2833087044ac7907ef910a5c5
tnimesh12345 <88110398+tnimesh12345@users.noreply.github.com>
2023-06-23 21:45:59 +0530

    after create tables

---
+++ node/proj1backend/backend/models/Client.js
@@ -0,0 +1,25 @@
+module.exports = (sequelize, DataTypes) => {
+  const Client = sequelize.define("Client", {
+    Name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    Address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    Contact: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    Email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    Password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+  return Client;
+};
